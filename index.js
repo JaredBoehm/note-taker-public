2549ac2e1b7dcb4bbcd37193d4d35eb190510c58
Jared Boehm <me@jaredboehm.com>
2023-09-07 18:22:26 -0600

    Update and rename server.js to index.js
    Working with railway

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const api = require('./routes/index.js')
 const path = require('path')
 
 const app = express()
-const PORT = process.env.port
+const PORT = process.env.port || 3001
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
@@ -13,4 +13,4 @@ app.use(express.static('public'))
 
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public/notes.html')))
 
-app.listen(PORT, () => console.log(`Live at ${PORT}`))
+app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}/`))
